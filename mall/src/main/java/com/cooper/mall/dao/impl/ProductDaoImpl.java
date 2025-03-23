d5a8c71b344465903ce7e0c40ce6c04d189b5b9d
Cooper <ghboris13@gmail.com>
2025-03-23 17:56:43 +0800

    商品功能 - 實作修改商品

--- mall/src/main/java/com/cooper/mall/dao/impl/ProductDaoImpl.java
+++ mall/src/main/java/com/cooper/mall/dao/impl/ProductDaoImpl.java
@@ -42,7 +42,7 @@ public class ProductDaoImpl implements ProductDao {
     public Integer createProduct(ProductRequest productRequest) {
 
         String sql = "INSERT INTO product(product_name, category, image_url, price, stock, description, created_date, last_modified_date) " +
-                    "VALUES (:productName, :category, :imageUrl, :price, :stock, :description, :createdDate, :lastModifiedDate); ";
+                    "VALUES (:productName, :category, :imageUrl, :price, :stock, :description, :createdDate, :lastModifiedDate) ";
 
         Map<String, Object> map  = new HashMap<String, Object>();
         map.put("productName", productRequest.getProductName());
@@ -62,4 +62,26 @@ public class ProductDaoImpl implements ProductDao {
 
         return productId;
     }
+
+    @Override
+    public void updateProduct(Integer productId, ProductRequest productRequest) {
+        String sql = " UPDATE product SET product_name = :productName, category = :category, " +
+                " image_url = :imageUrl, price = :price, stock = :stock, description = :description, " +
+                " last_modified_date = :lastModifiedDate " +
+                " where product_id = :productId ";
+        Map<String, Object> map = new HashMap<>();
+        map.put("productName", productRequest.getProductName());
+        map.put("category", productRequest.getCategory().toString());
+        map.put("imageUrl", productRequest.getImageUrl());
+        map.put("price", productRequest.getPrice());
+        map.put("stock", productRequest.getStock());
+        map.put("description", productRequest.getDescription());
+        Date now = new Date();
+        map.put("lastModifiedDate", now);
+        map.put("productId", productId);
+
+        namedParameterJdbcTemplate.update(sql, map);
+    }
+
+
 }
