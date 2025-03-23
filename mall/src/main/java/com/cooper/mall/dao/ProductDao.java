6243a8e3d3aa2df0ea4dbc0dc286fa0771e9c5ba
Cooper <ghboris13@gmail.com>
2025-03-24 01:05:22 +0800

    商品功能 - 實作查詢商品列表-基本架構

--- mall/src/main/java/com/cooper/mall/dao/ProductDao.java
+++ mall/src/main/java/com/cooper/mall/dao/ProductDao.java
@@ -3,7 +3,10 @@ package com.cooper.mall.dao;
 import com.cooper.mall.dto.ProductRequest;
 import com.cooper.mall.model.Product;
 
+import java.util.List;
+
 public interface ProductDao {
+    List<Product> getProducts();
     Product getProductById(Integer productId);
 
     Integer createProduct(ProductRequest productRequest);
