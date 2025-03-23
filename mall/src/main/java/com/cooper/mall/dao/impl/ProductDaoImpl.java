edf48a26cf4b9a54e89e2ac0ccc3b4b7cbce8947
Cooper <ghboris13@gmail.com>
2025-03-23 16:13:58 +0800

    商品功能 - 實作新增商品v2

--- mall/src/main/java/com/cooper/mall/dao/impl/ProductDaoImpl.java
+++ mall/src/main/java/com/cooper/mall/dao/impl/ProductDaoImpl.java
@@ -7,7 +7,6 @@ import com.cooper.mall.rowmapper.ProductRowMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
-import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Component;
