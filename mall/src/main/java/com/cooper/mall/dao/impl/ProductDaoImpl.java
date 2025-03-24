be70c035df3cc5b48b1362276f28e4c4d187d491
Cooper <ghboris13@gmail.com>
2025-03-24 14:48:32 +0800

    商品功能 - 實作查詢商品列表-分頁

--- mall/src/main/java/com/cooper/mall/dao/impl/ProductDaoImpl.java
+++ mall/src/main/java/com/cooper/mall/dao/impl/ProductDaoImpl.java
@@ -29,6 +29,7 @@ public class ProductDaoImpl implements ProductDao {
                 " created_date, last_modified_date " +
                 " FROM product WHERE 1=1 ";
         Map<String, Object> map = new HashMap<>();
+        //  查詢條件
         if (productQueryParams.getCategory() != null){
             sql = sql + " AND category = :category ";
             map.put("category", productQueryParams.getCategory().name());
@@ -37,7 +38,10 @@ public class ProductDaoImpl implements ProductDao {
             sql = sql + " AND product_name LIKE :search ";
             map.put("search", "%" + productQueryParams.getSearch() + "%" );
         }
-        sql = sql + " ORDER BY " + productQueryParams.getOredrBy() + " " + productQueryParams.getSort();
+        // 排序
+        sql = sql + " ORDER BY " + productQueryParams.getOrderBy() + " " + productQueryParams.getSort();
+        // 分頁
+        sql = sql + " LIMIT " + productQueryParams.getLimit() + " OFFSET " + productQueryParams.getOffset();
 
         List<Product> productList = namedParameterJdbcTemplate.query(sql, map, new ProductRowMapper());
         return productList;
