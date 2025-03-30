9db25237c467052151a31dc388ba55694fb4310b
Cooper <ghboris13@gmail.com>
2025-03-30 23:35:24 +0800

    帳號功能-檢查註冊的email，隱藏返回的密碼

--- mall/src/main/java/com/cooper/mall/dao/UserDao.java
+++ mall/src/main/java/com/cooper/mall/dao/UserDao.java
@@ -6,4 +6,6 @@ import com.cooper.mall.model.User;
 public interface UserDao {
     Integer createUser(UserRegisterRequest userRegisterRequest);
     User getUserById(Integer userId);
+
+    User getUserByEmail(String email);
 }
