2b945ececd20a620560a268c973cb7ee5d07b80c
Cooper <ghboris13@gmail.com>
2025-04-01 00:35:22 +0800

    帳號功能-使用MD5加密密碼

--- mall/src/main/java/com/cooper/mall/service/ProductServiceImpl/UserServerImpl.java
+++ mall/src/main/java/com/cooper/mall/service/ProductServiceImpl/UserServerImpl.java
@@ -10,6 +10,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.util.DigestUtils;
 import org.springframework.web.server.ResponseStatusException;
 
 @Component
@@ -24,6 +25,11 @@ public class UserServerImpl implements UserService {
     public Integer register(UserRegisterRequest userRegisterRequest) {
         // 檢查註冊的email
         User user = userDao.getUserByEmail(userRegisterRequest.getEmail());
+
+        // 使用 MD5 生成密碼的雜湊值
+        String hashedPassword = DigestUtils.md5DigestAsHex(userRegisterRequest.getPassword().getBytes());
+        userRegisterRequest.setPassword(hashedPassword);
+
         if (user != null){
             log.warn("該 email {} 已被註冊", userRegisterRequest.getEmail());
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
@@ -40,11 +46,16 @@ public class UserServerImpl implements UserService {
     @Override
     public User login(UserLoginRequest userLoginRequest) {
         User user = userDao.getUserByEmail(userLoginRequest.getEmail());
+        // 檢查user是否存在
         if (user == null){
             log.warn("該 email {} 尚未註冊", userLoginRequest.getEmail());
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
         }
-        if (user.getPassword().equals(userLoginRequest.getPassword())){
+        // 使用 MD5 生成密碼的雜湊值
+        String hashedPassword = DigestUtils.md5DigestAsHex(userLoginRequest.getPassword().getBytes());
+
+        // 檢查密碼
+        if (user.getPassword().equals(hashedPassword)){
             return user;
         } else {
             log.warn("該 email {} 密碼不正確", userLoginRequest.getEmail());
